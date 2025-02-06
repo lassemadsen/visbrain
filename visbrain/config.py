2518181b4f1687987b767adee8daa8a0736460a4
Lasse Stensvig Madsen <lasse.madsen93@gmail.com>
2025-02-06 17:00:50 +0100

    Moved vispy osmesa inside try

--- visbrain/config.py
+++ visbrain/config.py
@@ -4,14 +4,13 @@ import getopt
 import logging
 import os
 
-os.environ['VISPY_BACKEND'] = 'osmesa'
-os.environ['QT_QPA_PLATFORM'] = 'offscreen'
-os.environ["OMP_NUM_THREADS"] = "1"  # Avoid multithreading
 
 import vispy
 try:
     vispy.use(app='osmesa')
     assert vispy.app.use_app().backend_name == 'osmesa', 'Not using OSMesa'
+    os.environ['VISPY_BACKEND'] = 'osmesa'
+    os.environ["OMP_NUM_THREADS"] = "1"  # Avoid multithreading
     os.environ["QT_QPA_PLATFORM"] = "offscreen" # Set when plotting on headless cluster 
 except Exception as e:
     print("Error setting up vispy backend:", e)
